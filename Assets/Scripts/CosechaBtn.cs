680637aef56581bfbf3ec1dc20fe9dd370962744
A01750687 <117104186+A01750687@users.noreply.github.com>
2024-04-24 17:24:10 -0600

    tienda (venta)

---
+++ Assets/Scripts/CosechaBtn.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CosechaBtn : MonoBehaviour
+{
+    public ItemVenta itemV;
+    private TMP_Text precioText;
+
+    private void Awake()
+    {
+        precioText = transform.GetChild(0).GetComponent<TMP_Text>();
+        precioText.text = "$" + itemV.precioInicial.ToString();
+    }
+
+    public void vendeCosecha()
+    {
+        for (int i = 0; i < playerInventory.instance.slots.Length; i++)
+        {
+            if (playerInventory.instance.slots[i].GetComponent<Item>().itemVenta)
+            {
+                if (playerInventory.instance.slots[i].GetComponent<Item>().itemVenta.nombre == itemV.nombre)
+                {
+                    playerInventory.instance.slots[i].GetComponent<Item>().itemVenta = null;
+                    playerInventory.instance.slots[i].GetComponent<Image>().enabled = false;
+                    playerStats.instance.ganancia += itemV.precioInicial;
+                }
+            }
+        }
+    }
+}
