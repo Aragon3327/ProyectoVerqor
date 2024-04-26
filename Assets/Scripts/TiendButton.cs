ef430fceaa2d68661e615cbe82b4fac5873aab49
A01750687 <117104186+A01750687@users.noreply.github.com>
2024-04-26 10:10:54 -0600

    venta completa

--- Assets/Scripts/TiendButton.cs
+++ Assets/Scripts/TiendButton.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
 using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
 
 public class TiendButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public ItemTiendaObjt item;
+    public ItemVenta itemVenta;
 
     public void btnAccion(){
         gameObject.GetComponent<TiendButton>().OnTiendaButtonClick();
@@ -21,6 +23,22 @@ public class TiendButton : MonoBehaviour
                 if(!playerInventory.instance.isFull[i]){
                     playerInventory.instance.slots[i].GetComponent<Item>().item = item;
                     playerInventory.instance.slots[i].GetComponent<Item>().UpdateItem();
+                    playerInventory.instance.isFull[i] = true;
+                    playerStats.instance.ganancia -= item.precio;
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public void OnTiendButtonClick(){
+
+        if(playerStats.instance.ganancia >= item.precio){
+            for(int i = 0; i < playerInventory.instance.slots.Length;i++){
+                if(!playerInventory.instance.isFull[i]){
+                    playerInventory.instance.slots[i].GetComponent<Item>().itemVenta = itemVenta;
+                    playerInventory.instance.slots[i].GetComponent<Item>().UpdateVenta();
                     playerInventory.instance.isFull[i] = true;
                     playerStats.instance.ganancia -= item.precio;
                     break;
